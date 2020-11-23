ac19c93f4fb2f32c4de5ab653cef3441e840f0dd
sun <964273641@qq.com>
2020-11-23 18:14:40 +0800

    add ad

--- components/header.js
+++ components/header.js
@@ -22,7 +22,6 @@ const Header = () => {
           <meta name='viewport' content='width=device-width, initial-scale=1.0' />
           <title>Think before you speak. Read before you think.｜Jarvis Sun</title>
           <link rel='icon' href='/favicon.ico' type='image/x-icon' />
-
         </Head>
         <Head>
           <script data-ad-client='ca-pub-9856877633666184' async src='https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js'></script>
