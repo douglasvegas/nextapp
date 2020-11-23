97da50f0866d571736c5e23eae71cd5d9e7e84b2
sun <964273641@qq.com>
2020-11-23 18:35:07 +0800

    edit

--- components/layout.js
+++ components/layout.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import Router from 'next/router';
 import NProgress from 'nprogress';
 import Head from 'next/head';
-import { initGA, logPageView } from './ga.js';
+// import { initGA, logPageView } from './ga.js';
 
 
 
@@ -15,11 +15,11 @@ Router.events.on('routeChangeError', () => NProgress.done());
 
 export default class Layout extends Component {
     componentDidMount () {
-        if (!window.GA_INITIALIZED) {
-            initGA();
-            window.GA_INITIALIZED = true
-        }
-        logPageView()
+        // if (!window.GA_INITIALIZED) {
+        //     initGA();
+        //     window.GA_INITIALIZED = true
+        // }
+        // logPageView()
     }
 
     render () {
